c0b3832e2c890bed5ab01fa6ba5bb9b9a8d2a7c5
Samuel Hagen <124194298+sagen704@users.noreply.github.com>
2026-07-06 11:00:38 -0600

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+
+        res = [0] * len(temperatures)
+
+        stack = []
+
+        for i in range(len(temperatures)):
+
+            while len(stack) != 0 and temperatures[i] > stack[-1][0]:
+                found_higher = stack.pop()
+                print(f"found higher {found_higher}")
+                
+                # updating the location
+                res[found_higher[1]] = i - found_higher[1]
+            stack.append([temperatures[i], i])
+
+        return res
+        
